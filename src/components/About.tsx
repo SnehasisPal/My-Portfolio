42997ccc46f13d243efd86e9c15635ba84e165bc
SnehasisPal <96253792+SnehasisPal@users.noreply.github.com>
2025-01-20 11:30:20 +0530

    Add files via upload

---
+++ src/components/About.tsx
@@ -0,0 +1,21 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          Dynamic Full Stack Developer with 3 years of experience in web and mobile application development. Proficient in   
+designing and implementing full backend databases and frontend web applications. Proven track record in developing 
+and deploying high-impact projects, driving innovation, and enhancing application performance.
+        </p>
+      </div>
+    </div>
+  );
+};
+//  Combines strong 
+// development skills with extensive experience in agile methodologies and analytical abilities to deliver robust and scalable 
+// solutions. Dedicated to continuously learning and researching new trending technologies
+
+export default About;
